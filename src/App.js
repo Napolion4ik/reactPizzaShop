df68b55266862999849b8c24d57a07a987855230
Napolion4ik <dziubaoleg20@gmail.com>
2025-09-16 00:43:23 +0300

    Виправлення REST API піцц

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { Header } from "./components/index";
 import { Home, Cart } from "./pages/index";
 import { Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 function App() {
-    return (
-        <div className="wrapper">
-            <Header />
-            <div className="content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/cart.html" element={<Cart />} />
-                </Routes>
-            </div>
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart.html" element={<Cart />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/CartIsFilled.js
+++ src/components/CartIsFilled.js
@@ -3,110 +3,108 @@ import { useDispatch, useSelector } from "react-redux";
 
 import CartItem from "../components/CartItem";
 import {
-    cleanUpCart,
-    addPizzaToCart,
-    minusOnePizza,
-    deletePizza,
+  cleanUpCart,
+  addPizzaToCart,
+  minusOnePizza,
+  deletePizza,
 } from "../redux/action/cart";
 
 const CartIsFilled = () => {
-    const { items, totalPrice, totalCount } = useSelector(
-        (state) => state.cart
-    );
-    const dispatch = useDispatch();
+  const { items, totalPrice, totalCount } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
-    const pizzasGroup = Object.keys(items).map((key) => (
-        <CartItem
-            pizza={items[key][0]}
-            name={items[key][0].name}
-            type={items[key][0].type}
-            size={items[key][0].size}
-            img={items[key][0].imageUrl}
-            pizzaCount={items[key].length}
-            price={items[key].reduce((prev, item) => {
-                return prev + item.price;
-            }, 0)}
-            handlerAddOnePizza={handlerAddOnePizza}
-            handlerMinusOnePizza={handlerMinusOnePizza}
-            handlerDeletePizza={handlerDeletePizza}
-        />
-    ));
-    const handlerCleanUp = () => {
-        if (window.confirm("Ви дійсно хочите створити чорну діру?")) {
-            dispatch(cleanUpCart());
-        }
-    };
-    function handlerAddOnePizza(plusPizza) {
-        dispatch(addPizzaToCart(plusPizza));
+  const pizzasGroup = Object.keys(items).map((key) => (
+    <CartItem
+      pizza={items[key][0]}
+      name={items[key][0].name}
+      type={items[key][0].type}
+      size={items[key][0].size}
+      img={items[key][0].imageUrl}
+      pizzaCount={items[key].length}
+      price={items[key].reduce((prev, item) => {
+        return prev + item.price;
+      }, 0)}
+      handlerAddOnePizza={handlerAddOnePizza}
+      handlerMinusOnePizza={handlerMinusOnePizza}
+      handlerDeletePizza={handlerDeletePizza}
+    />
+  ));
+  const handlerCleanUp = () => {
+    if (window.confirm("Ви дійсно хочите створити чорну діру?")) {
+      dispatch(cleanUpCart());
     }
-    function handlerMinusOnePizza(minusPizza) {
-        dispatch(minusOnePizza(minusPizza));
-    }
-    function handlerDeletePizza(minusPizza) {
-        dispatch(deletePizza(minusPizza));
-    }
-    return (
-        <div className="cart">
-            <div className="cart__top">
-                <h2 className="content__title">Корзина</h2>
-                <div onClick={handlerCleanUp} className="cart__clear">
-                    <svg
-                        width="18"
-                        height="18"
-                        viewBox="0 0 18 18"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
-                            stroke="white"
-                            stroke-width="1.8"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                        <path
-                            d="M14.3333 16.3333C15.0697 16.3333 15.6667 15.7364 15.6667 15C15.6667 14.2636 15.0697 13.6667 14.3333 13.6667C13.597 13.6667 13 14.2636 13 15C13 15.7364 13.597 16.3333 14.3333 16.3333Z"
-                            stroke="white"
-                            stroke-width="1.8"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                        <path
-                            d="M4.78002 4.99999H16.3334L15.2134 10.5933C15.1524 10.9003 14.9854 11.176 14.7417 11.3722C14.4979 11.5684 14.1929 11.6727 13.88 11.6667H6.83335C6.50781 11.6694 6.1925 11.553 5.94689 11.3393C5.70128 11.1256 5.54233 10.8295 5.50002 10.5067L4.48669 2.82666C4.44466 2.50615 4.28764 2.21182 4.04482 1.99844C3.80201 1.78505 3.48994 1.66715 3.16669 1.66666H1.66669"
-                            stroke="white"
-                            stroke-width="1.8"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                    </svg>
+  };
+  function handlerAddOnePizza(plusPizza) {
+    dispatch(addPizzaToCart(plusPizza));
+  }
+  function handlerMinusOnePizza(minusPizza) {
+    dispatch(minusOnePizza(minusPizza));
+  }
+  function handlerDeletePizza(minusPizza) {
+    dispatch(deletePizza(minusPizza));
+  }
+  return (
+    <div className="cart">
+      <div className="cart__top">
+        <h2 className="content__title">Корзина</h2>
+        <div onClick={handlerCleanUp} className="cart__clear">
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
+              stroke="white"
+              stroke-width="1.8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M14.3333 16.3333C15.0697 16.3333 15.6667 15.7364 15.6667 15C15.6667 14.2636 15.0697 13.6667 14.3333 13.6667C13.597 13.6667 13 14.2636 13 15C13 15.7364 13.597 16.3333 14.3333 16.3333Z"
+              stroke="white"
+              stroke-width="1.8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M4.78002 4.99999H16.3334L15.2134 10.5933C15.1524 10.9003 14.9854 11.176 14.7417 11.3722C14.4979 11.5684 14.1929 11.6727 13.88 11.6667H6.83335C6.50781 11.6694 6.1925 11.553 5.94689 11.3393C5.70128 11.1256 5.54233 10.8295 5.50002 10.5067L4.48669 2.82666C4.44466 2.50615 4.28764 2.21182 4.04482 1.99844C3.80201 1.78505 3.48994 1.66715 3.16669 1.66666H1.66669"
+              stroke="white"
+              stroke-width="1.8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
 
-                    <span>Очистити корзину</span>
-                </div>
-            </div>
-            <div className="content__items">{pizzasGroup}</div>
-            <div className="cart__bottom">
-                <div className="cart__bottom-details">
-                    <span>
-                        Всього піцц: <b>{totalCount} шт.</b>{" "}
-                    </span>
-                    <span>
-                        Сума замовлення: <b>{totalPrice} ₽</b>{" "}
-                    </span>
-                </div>
-                <div className="cart__bottom-buttons">
-                    <Link
-                        to="/"
-                        className="button button--outline button--add go-back-btn"
-                    >
-                        <span>Повернутися назад</span>
-                    </Link>
-                    <div className="button pay-btn">
-                        <span>Заплатити зараз</span>
-                    </div>
-                </div>
-            </div>
+          <span>Очистити корзину</span>
         </div>
-    );
+      </div>
+      <div className="content__items">{pizzasGroup}</div>
+      <div className="cart__bottom">
+        <div className="cart__bottom-details">
+          <span>
+            Всього піцц: <b>{totalCount} шт.</b>{" "}
+          </span>
+          <span>
+            Сума замовлення: <b>{totalPrice} грн</b>{" "}
+          </span>
+        </div>
+        <div className="cart__bottom-buttons">
+          <Link
+            to="/"
+            className="button button--outline button--add go-back-btn"
+          >
+            <span>Повернутися назад</span>
+          </Link>
+          <div className="button pay-btn">
+            <span>Заплатити зараз</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartIsFilled;
